7fabc3ae3d3cb600ae67e3e3b98933ba6b0974cb
megan_s <m.k.schacherbauer@gmail.com>
2022-01-12 15:50:08 -0600

    Addjusted components of Plans view, light styling on plans carousel

--- src/components/PlansCarousel.vue
+++ src/components/PlansCarousel.vue
@@ -1,42 +1,69 @@
 <template>
-    <div class="plans-carousel d-flex justify-content-center m-5">
+  <div class="plans-carousel d-flex justify-content-center mt-4">
+      <div class="card l-card p-4 align-self-center">
+        <div class="card-body">
+          <h3 class="card-title">Placeholder</h3>
+          <bundle-logos></bundle-logos>
+          <price></price>
+          <p class="card-text">Description of bundle</p>
+          <button class="btn btn-outline-dark w-100">Select</button>
+      </div>
+    </div>
 
-      <div class="l-card">
-        <plans-panel></plans-panel>
+ 
+      <div class="card frnt-card p-4 align-self-center">
+        <div class="card-body">
+          <h3 class="card-title">Placeholder</h3>
+          <bundle-logos></bundle-logos>
+          <price></price>
+          <p class="card-text">Description of bundle</p>
+          <button class="btn btn-dark w-100">Select</button>
+        </div>
       </div>
 
-      <div class="frnt-card">
-        <plans-panel></plans-panel>
-      </div>
 
-      <div class="r-card">
-        <plans-panel></plans-panel>
+      <div class="card r-card p-4 align-self-center">
+        <div class="card-body">
+          <h3 class="card-title">Placeholder</h3>
+          <bundle-logos></bundle-logos>
+          <price></price>
+          <p class="card-text">Description of bundle</p>
+          <button class="btn btn-outline-dark w-100">Select</button>
+        </div>
       </div>
     </div>
 </template>
 
 <script>
-import PlansPanel from "./PlansPanel.vue";
+import BundleLogos from "./BundleLogos.vue";
+import Price from "./Price.vue";
 
 export default {
   name: "PlansCarousel",
   components: {
-    PlansPanel,
+    BundleLogos,
+    Price,
   },
 };
 </script>
 
 <style>
 
-.frnt-card{
-    z-index: 1;
+.frnt-card, .l-card, .r-card{
+  width: 33.333334%;
+}
+.frnt-card {
+  z-index: 1;
+  height: 450px;
 }
 
-.l-card{
+.l-card {
   margin-right: -15px;
+    height: 400px;
 }
 
-.r-card{
+.r-card {
   margin-left: -15px;
+    height: 400px;
 }
 </style>
