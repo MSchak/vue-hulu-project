efc5329a936f1507019ce2039d3a6ef0fba5f643
megan_s <m.k.schacherbauer@gmail.com>
2022-04-25 14:14:18 -0500

    Added hover and focus states to main navigation items

--- src/components/MainNavigation.vue
+++ src/components/MainNavigation.vue
@@ -3,11 +3,19 @@
     <div :class="navClass" class="container-fluid px-md-4">
       <div class="row align-items-center h-100 mx-1 me-4 pt-1 px-xl-2">
         <div class="col-3">
-          <p class="title mb-0">hulu</p>
+          <p class="title mb-0">
+            <router-link to="/welcome">hulu</router-link>
+          </p>
         </div>
 
         <div class="col-6 text-center nav-items">
-          <select :class="select" name="menu" id="menu" v-model="selected" @change="navigateTo">
+          <select
+            :class="select"
+            name="menu"
+            id="menu"
+            v-model="selected"
+            @change="navigateTo"
+          >
             <option value="1">Welcome</option>
             <option value="2">Plans</option>
             <option value="3">Home</option>
@@ -64,29 +72,39 @@ export default {
   color: white;
 }
 
-.nav-container-dark{
-    position: fixed;
+.nav-container-dark {
+  position: fixed;
   border-bottom: 0.5px solid white;
   z-index: 100;
   height: 60px;
   color: black;
-  background-color:white;
+  background-color: white;
 }
 
-.title {
+.title a {
   font-family: "Graphik Black";
   font-size: 1.5em;
   letter-spacing: 3px;
   z-index: 101;
+  color: white;
+  text-decoration: none;
 }
 
- .select{
+.title a:hover {
+  font-size: 160%;
+}
+
+.title a:focus {
+  border: 1px solid blue;
+}
+
+.select {
   border: none;
   width: 70px;
   padding: 10px 4px;
   border-radius: 4px;
-      background-color: #00000014;
-  opacity: .9;
+  background-color: #00000014;
+  opacity: 0.9;
   font-family: "Graphik Regular";
   font-weight: bold;
   text-transform: uppercase;
@@ -100,8 +118,8 @@ export default {
   width: 70px;
   padding: 10px 4px;
   border-radius: 4px;
-      background-color: #00000014;
-  opacity: .9;
+  background-color: #00000014;
+  opacity: 0.9;
   font-family: "Graphik Regular";
   font-weight: bold;
   text-transform: uppercase;
@@ -109,8 +127,6 @@ export default {
   letter-spacing: 2px;
   color: black;
 }
-
-
 
 .nav-items {
   font-family: "Graphik Regular";
@@ -130,7 +146,7 @@ export default {
   color: white;
 }
 
-.nav-item-dark{
+.nav-item-dark {
   font-family: "Graphik Medium";
   text-decoration: none;
   font-size: 12px;
@@ -139,6 +155,14 @@ export default {
 
 .nav-items ul li {
   list-style-type: none;
+  padding: 10px;
+  border-radius: 10px;
+}
+.nav-items ul li:hover {
+  background-color: rgba(221, 221, 221, 0.527);
+}
+.nav-items ul li:focus {
+  border: 1px solid blue;
 }
 
 @media (min-width: 768px) {
@@ -154,7 +178,7 @@ export default {
     font-size: 14px;
   }
 
-  .nav-item-dark{
+  .nav-item-dark {
     font-size: 14px;
   }
 }
